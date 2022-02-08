c46b0586bdc0e8dfa9174df3e0c5c6657935c22a
JonSverre <jon.sverre.karlsen@nav.no>
2022-02-08 16:48:23 +0100

    her er koden litt tungvinn

---
+++ src/oppgave_6.ps1
@@ -0,0 +1,80 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $UrlKortstokk
+)
+
+Clear-Host
+$ErrorActionPreference = "Stop"
+
+function kortstokkstringfunc {[OutputType([String])]
+param ([object[]]$kortstokk)
+
+$kortstokksjekk = ""
+
+foreach ($kort in $kortstokk) {
+    $kortstokksjekk = $kortstokksjekk + "$($kort.suit[0])" + "$($kort.value)" + ","
+  
+}
+$kortstokksjekk = "$($kortstokksjekk.Substring(0,$kortstokksjekk.Length-1))"
+
+return $kortstokksjekk
+}
+
+function kortstokksumfunc {[OutputType([Int])]
+    param ([object[]]$kortstokk)
+    
+    $kortstokksum = 0
+    
+
+    foreach ($kort in $kortstokk) {
+        
+        if ($kort.value -eq "A") {
+            $kortstokksum = $kortstokksum + 11
+                      
+        }
+        elseif ($kort.value -eq "K") {
+            $kortstokksum = $kortstokksum + 10
+           
+        }
+        elseif ($kort.value -eq "Q") {
+            $kortstokksum = $kortstokksum + 10
+           
+        }
+        elseif ($kort.value -eq "J") {
+            $kortstokksum = $kortstokksum + 10
+            
+        }
+        else {
+            $kortstokksum = $kortstokksum + $kort.value  
+        }
+    }
+        
+        return $kortstokksum
+
+
+}
+    
+try {
+    $webrequest = Invoke-WebRequest -uri $UrlKortstokk
+   
+}
+catch {
+    Write-Error "Denne Url har ikke noen kortstokk" -ErrorAction Stop
+}
+
+
+$kortstokk = ""
+$kortstokk = $webrequest.Content | ConvertFrom-Json
+Write-Output "Kortstokk: $(kortstokkstringfunc -kortstokk $kortstokk)"
+Write-Output "Poengsum: $(kortstokksumfunc -kortstokk $kortstokk)"
+
+$kortstokk = $(kortstokkstringfunc -kortstokk $kortstokk) -split (",")
+$meg = $kortstokk[0..1] 
+Write-Output "Meg: $($meg -join (","))" 
+$kortstokk = $kortstokk[2..$kortstokk.count]
+$magnus = $kortstokk[0..1]
+Write-Output "Magnus $($magnus -join (","))" 
+$kortstokk = $kortstokk[2..$kortstokk.count]
+Write-Output "Kortstokk: $($kortstokk -join (","))"
